b625216caeb66a4a0f221b65a4b9dbc9afbde91d
Boussax <adrien.boussaoud@gmail.com>
2024-01-31 22:21:26 +0100

    feat: add delete button to details view

--- src/views/FermentDetailsView.vue
+++ src/views/FermentDetailsView.vue
@@ -2,6 +2,7 @@
 import { ref, onMounted } from 'vue'
 import FermentService from '@/services/FermentService'
 import type { Ferment } from '@/assets/models/ferment'
+import router from '@/router'
 
 const props = defineProps({
   id: {
@@ -16,6 +17,15 @@ onMounted(() => {
     .then((response) => (ferment.value = response.data))
     .catch((error) => console.log(error))
 })
+
+function deleteCurrentFerment() {
+  FermentService.deleteFerment(ferment.value!.id)
+    .then((response) => {
+      console.log(response)
+      router.push({ name: 'dashboard' })
+    })
+    .catch((error) => console.log(error))
+}
 </script>
 
 <template>
@@ -23,5 +33,6 @@ onMounted(() => {
     <h1>{{ ferment.name }}</h1>
     <p>{{ ferment.startDate }}</p>
     <p>{{ ferment.type }}</p>
+    <button @click="deleteCurrentFerment">Delete</button>
   </div>
 </template>
